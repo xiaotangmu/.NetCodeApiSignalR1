5dbbf3ac07a9161e028ed5fdf2f38d1ffae6482d
xiaotangmu <1275485428@qq.com>
2020-11-20 16:15:48 +0800

    xxxx

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -8,6 +8,7 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        //private static ConcurrentDictionary<string, string> clientMap = new ConcurrentDictionary<string, string>();
 
         /// <summary>
         /// 给所有客户端发送消息
@@ -20,13 +21,76 @@ namespace SignalRChat.Hubs
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
         /// <summary>
-        /// 向调用客户端发送消息（备用）
+        /// 向调用客户端发送消息 -- 发给自己（备用）
         /// </summary>
         /// <param name="message"></param>
         /// <returns></returns>
         public async Task SendMessageCaller(string message)
         {
+            // 两种方式
             await Clients.Caller.SendAsync("ReceiveCaller", message);
+            //return Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+        }
+        /// <summary>
+        /// 向指定Id推送消息
+        /// </summary>
+        /// <param name="userid">要推送消息的对象</param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task Echo(string userid, string message)
+        {
+            await Clients.Client(userid).SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", $"{Context.ConnectionId} TO {userid}: {message}");
+        }
+        /// <summary>
+        /// 向指定组推送消息
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Task SendToGroup(string groupName, string message)
+        {
+            return Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+        /// <summary>
+        /// 加入指定组并向组推送消息
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} joined {groupName}");
+        }
+        /// <summary>
+        /// 推出指定组并向组推送消息
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} left {groupName}");
+        }
+        /// <summary>
+        /// 建立连接时触发
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            // 获取路径参数
+            //string userName = Context.GetHttpContext().Request.Query["username"];
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} joined");
+        }
+
+        /// <summary>
+        /// 离开连接时触发
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} left");
         }
     }
 }
